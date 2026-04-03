90840072928648255338ee6b25100ec7babd78ef
Cristian Rubens <cristian.rubens2006@gmail.com>
2026-04-03 19:57:11 -0300

    Update Ponto.java

--- Roteiro02/src/main/java/Classes/Ponto.java
+++ Roteiro02/src/main/java/Classes/Ponto.java
@@ -1,6 +1,15 @@
-package Classes;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
 import java.util.Scanner;
 
+/**
+ *
+ * @author tbona
+ */
 public class Ponto {
     
     private double x;
