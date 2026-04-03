eee42aa86f6925c6d0eefa674d2b73ea93912c8a
Cristian Rubens <cristian.rubens2006@gmail.com>
2026-04-03 17:11:08 -0300

    Update Quadrado.java

--- Roteiro02/src/main/java/Classes/Quadrado.java
+++ Roteiro02/src/main/java/Classes/Quadrado.java
@@ -88,4 +88,36 @@ public class Quadrado {
         }
         return false;
     }
+
+    public Ponto getP1() {
+        return p1;
+    }
+
+    public void setP1(Ponto p1) {
+        this.p1 = p1;
+    }
+
+    public Ponto getP2() {
+        return p2;
+    }
+
+    public void setP2(Ponto p2) {
+        this.p2 = p2;
+    }
+
+    public Ponto getP3() {
+        return p3;
+    }
+
+    public void setP3(Ponto p3) {
+        this.p3 = p3;
+    }
+
+    public Ponto getP4() {
+        return p4;
+    }
+
+    public void setP4(Ponto p4) {
+        this.p4 = p4;
+    }
 }
