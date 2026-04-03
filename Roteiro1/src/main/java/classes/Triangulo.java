1bb9243159f400bb9f47f2ba1fe23723dd7440bb
Thierry Bonato <bonatothierry10@gmail.com>
2026-04-03 19:18:54 -0300

    Atualização dos Códigos da Main e Triangulo

--- Roteiro1/src/main/java/classes/Triangulo.java
+++ Roteiro1/src/main/java/classes/Triangulo.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package classes;
 
-/**
- *
- * @author tbona
- */
+package classes;
+import classes.Ponto;
+
 public class Triangulo {
     
     private Ponto p1;
@@ -19,29 +13,67 @@ public class Triangulo {
         this.p2 = p2;
         this.p3 = p3;
     }
+
+    public Triangulo(Ponto p1, Ponto p2, Ponto p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
     
-    public void fill(){
-        
+    public void imprimir(){
+        System.out.println("Os Pontos são:");
+        System.out.println("Ponto A: " + p1);
+        System.out.println("Ponto B: " + p2);
+        System.out.println("Ponto C: " + p3);
     }
     
     public boolean isColinear(Ponto pB, Ponto pC){
-        if(this.Determinante(pB, pC) == 0){
+        if(p1.Determinante(pB, pC) == 0){
             return true;
         }else{
             return false;
         }
     }
     
-    public void perimetro(){
+    public double perimetro(){
         
+        if(isColinear(p2, p3)){
+            System.out.println("Não tem Perímetro.");
+            return 0;
+        }
+        
+        double distAB = p1.distanciaDoisPontos(p2);
+        double distBC = p2.distanciaDoisPontos(p3);
+        double distAC = p3.distanciaDoisPontos(p1);
+        
+        return distAB + distBC + distAC;
+    }
+    
+    public String tipo(){
+        
+        if(isColinear(p2, p3)){
+            return "Não é um Triângulo!";
+        }
+        
+        double distAB = p1.distanciaDoisPontos(p2);
+        double distBC = p2.distanciaDoisPontos(p3);
+        double distAC = p3.distanciaDoisPontos(p1);
+        
+        if(distAB == distBC && distBC == distAC){
+            return "Equilátero";
+        }else if(distAB == distBC || distBC == distAC){
+            return "Isósceles";
+        }else{
+            return "Escaleno";
+        }
     }
     
     public double area(Ponto pB, Ponto pC){
-        double det = this.Determinante(pB, pC);
+        double det = p1.Determinante(pB, pC);
         
         double areaTriang = 0.0;
         if(det != 0){
-            areaTriang = det/2.0;
+            areaTriang = Math.abs(det)/2.0;
         }
         return areaTriang;
     }
--- Roteiro1/src/main/java/com/mycompany/roteiro1/Roteiro1.java
+++ Roteiro1/src/main/java/com/mycompany/roteiro1/Roteiro1.java
@@ -1,32 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.roteiro1;
 
 import classes.Ponto;
+import classes.Triangulo;
 
-/**
- *
- * @author tbona
- */
+
 public class Roteiro1 {
 
     public static void main(String[] args) {
         
-        Ponto pA = new Ponto(4, 7);
-        System.out.println(pA.toString());
+        Ponto pA = new Ponto(1, 2);
+       
+        Ponto pB = new Ponto(3, 2);
         
-        Ponto pB = new Ponto(7, 9);
-        System.out.println(pB.toString());
+        Ponto pC = new Ponto(1, 1);
         
-        Ponto pC = new Ponto(5, 6);
-        System.out.println(pC.toString());
+        
+        Triangulo T1 = new Triangulo(pA, pB, pC);
+        
+        T1.imprimir();
         
         System.out.println("A distância do Ponto A e B: " + pA.distanciaDoisPontos(pB));
         System.out.println("A distância do Ponto A e C: " + pA.distanciaDoisPontos(pC));
         System.out.println("A distância do Ponto B e C: " + pB.distanciaDoisPontos(pC));
         
-        System.out.println("A área do triângulo é: " + pA.area(pB, pC));
+        System.out.println("A área do triângulo é: " + T1.area(pB, pC));
+        System.out.println("O Tipo do Triângulo é: " + T1.tipo());
+        System.out.println("O Perímetro do Triângulo é: " + T1.perimetro());
     }
 }
